3703b08ffe8731546f1ac309ab30983e6038d135
Chidera <okekechidera97@gmail.com>
2020-11-20 17:28:44 +0100

    added extra state and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import SessionsAndBreaks from "./components/SessionsAndBreaks";
 
 function App() {
-	return <div className="App"></div>;
+	const [breaks, setBreaks] = useState(5);
+	const [sessions, setSessions] = useState(25);
+
+	return (
+		<div className="container">
+			<h1>Pomodoro Clock</h1>
+			<SessionsAndBreaks breaks={breaks} sessions={sessions} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/SessionsAndBreaks.jsx
+++ src/components/SessionsAndBreaks.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function SessionsAndBreaks() {
-	return <div></div>;
+function SessionsAndBreaks({ breaks, sessions }) {
+	return (
+		<div>
+			<div className="breaks">
+				<h2>Break Length</h2>
+				<h2>{breaks}</h2>
+			</div>
+			<div className="session">
+				<h2>Session Length</h2>
+				<h2>{sessions}</h2>
+			</div>
+		</div>
+	);
 }
 
 export default SessionsAndBreaks;
