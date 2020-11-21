ae5686187bc17eb9e6dc229a8bb4cbfa0ee7b202
Chidera <okekechidera97@gmail.com>
2020-11-21 18:44:17 +0100

    added extra functionality and logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+	faCheckSquare,
+	faCoffee,
+	faArrowUp,
+	faArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import "./App.css";
 import SessionsAndBreaks from "./components/SessionsAndBreaks";
 
+library.add(faCheckSquare, faCoffee, faArrowUp, faArrowDown);
 function App() {
 	const [breaks, setBreaks] = useState(5);
 	const [sessions, setSessions] = useState(25);
 
+	const incrementSession = () => {
+		if (sessions <= 59) {
+			setSessions((sessions) => sessions + 1);
+		}
+	};
+
+	const incrementBreak = () => {
+		if (breaks <= 19) {
+			setBreaks((breaks) => breaks + 1);
+		}
+	};
+
+	const decrementSession = () => {
+		if (sessions >= 2) {
+			setSessions((sessions) => sessions - 1);
+		}
+	};
+	const decrementBreak = () => {
+		if (breaks >= 2) {
+			setBreaks((breaks) => breaks - 1);
+		}
+	};
+
 	return (
 		<div className="container">
 			<h1>Pomodoro Clock</h1>
-			<SessionsAndBreaks breaks={breaks} sessions={sessions} />
+			<SessionsAndBreaks
+				breaks={breaks}
+				sessions={sessions}
+				incrementSession={incrementSession}
+				incrementBreak={incrementBreak}
+				decrementBreak={decrementBreak}
+				decrementSession={decrementSession}
+			/>
 		</div>
 	);
 }
--- src/components/SessionsAndBreaks.jsx
+++ src/components/SessionsAndBreaks.jsx
@@ -1,15 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-function SessionsAndBreaks({ breaks, sessions }) {
+function SessionsAndBreaks({
+	breaks,
+	sessions,
+	incrementBreak,
+	incrementSession,
+	decrementBreak,
+	decrementSession,
+}) {
 	return (
 		<div>
 			<div className="breaks">
 				<h2>Break Length</h2>
-				<h2>{breaks}</h2>
+				<div className="icons">
+					<FontAwesomeIcon onClick={incrementBreak} icon="arrow-up" size="3x" />
+					<h2>{breaks}</h2>
+					<FontAwesomeIcon
+						onClick={decrementBreak}
+						icon="arrow-down"
+						size="3x"
+					/>
+				</div>
 			</div>
 			<div className="session">
 				<h2>Session Length</h2>
-				<h2>{sessions}</h2>
+				<div className="icons">
+					<FontAwesomeIcon
+						onClick={incrementSession}
+						icon="arrow-up"
+						size="3x"
+					/>
+					<h2>{sessions}</h2>
+					<FontAwesomeIcon
+						onClick={decrementSession}
+						icon="arrow-down"
+						size="3x"
+					/>
+				</div>
 			</div>
 		</div>
 	);
