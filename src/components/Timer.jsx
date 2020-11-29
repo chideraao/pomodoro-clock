d63df68691a75211392e9fd6bf9c06dc0564d6aa
Chidera <okekechidera97@gmail.com>
2020-11-29 12:23:15 +0100

    updated timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,40 +1,48 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Timer = ({ sessions, breaks }) => {
-	const [minutes, setMinutes] = useState(sessions);
-	const [seconds, setSeconds] = useState(0);
-	const [onSession, setOnSession] = useState(true);
+	let totalSecs = sessions * 60;
+	let intervalHandle;
+
+	const [timeLeft, setTimeLeft] = useState(totalSecs);
+	const [onSession, setonSession] = useState(true);
+	const [minutes, setMinutes] = useState("");
+
+	let min = Math.floor(timeLeft / 60);
+	let sec = timeLeft - min * 60;
+	console.log(min);
+	console.log(sec);
+
+	const tick = () => {
+		setMinutes(min);
+		setTimeLeft(sec);
+		setTimeLeft(timeLeft - 1);
+
+		if (sec < 10) {
+			setTimeLeft("0" + timeLeft);
+		}
+		if (min < 10) {
+			setMinutes("0" + min);
+		}
+		if ((min === 0) & (sec === 0)) {
+			clearInterval(intervalHandle);
+		}
+	};
 
 	const startCount = () => {
-		let myInterval = setInterval(() => {
-			if (seconds > 0) {
-				setSeconds(seconds - 1);
-			}
-			if (seconds === 0) {
-				if (minutes === 0) {
-					clearInterval(myInterval);
-					setOnSession(false);
-				} else {
-					setMinutes(minutes - 1);
-					setSeconds(59);
-				}
-			}
-		}, 1000);
-		return () => {
-			clearInterval(myInterval);
-		};
+		intervalHandle = setInterval(tick, 1000);
+		/*let time = this.state.minutes;
+		this.secondsRemaining = time * 60;*/
 	};
 
 	return (
 		<div>
 			<h2>{onSession ? "Session" : "Break"}</h2>
 
-			{minutes === 0 && seconds === 0 ? null : (
+			{minutes === 0 && timeLeft === 0 ? null : (
 				<h1>
 					{" "}
-					{minutes < 10 ? `0${minutes}` : minutes}:
-					{seconds < 10 ? `0${seconds}` : seconds}
+					{min < 10 ? `0${min}` : min}:{sec < 10 ? `0${sec}` : sec}
 				</h1>
 			)}
 			<button onClick={startCount}>Start</button>
